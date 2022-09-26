f9be846aebcc80157c773c0deb082b79018a0685
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-09-26 16:58:04 +0200

    Initial commit

---
+++ src/test/java/hello/GreeterTest.java
@@ -0,0 +1,16 @@
+package hello;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+public class GreeterTest {
+
+    private Greeter greeter = new Greeter();
+
+    @Test
+    public void greeterSaysHello() {
+        assertThat(greeter.sayHello(), containsString("Hello"));
+    }
+
+}
